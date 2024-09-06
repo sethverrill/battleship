380088a3c3ea77f4ab991cfe3abd61e162f8000c
Alora Riley <alora.riley@gmail.com>
2024-09-05 18:50:30 -0600

    feat: Add place_ship method and update empty method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,17 @@
 class Cell
-    attr_reader :coordinate
+    attr_reader :coordinate,
+                :ship
     
     def initialize(coordinate)
         @coordinate = coordinate 
+        @ship = nil
+    end
+
+    def place_ship(ship)
+        @ship = ship
+    end
+
+    def empty?
+        @ship.nil?
     end
 end
