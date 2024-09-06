b74baca8f58053bc91b19fe9feb169c38f877545
Alora Riley <alora.riley@gmail.com>
2024-09-05 18:18:41 -0600

    feat: Add health command with starting health 3

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,15 @@
 class Ship
-    attr_reader :name
-                :length
+    attr_reader :name,
+                :length,
+                :health
     def initialize(name, length)
         @name = name
         @length = length
+        @starting_health = 3
+        @health = health
+    end
+
+    def health
+        @starting_health
     end
 end
