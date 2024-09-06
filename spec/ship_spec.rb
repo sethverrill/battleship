358a002ef2347ee10f22bcb54c4dac7c2a0aa47b
Alora Riley <alora.riley@gmail.com>
2024-09-05 18:08:05 -0600

    fix: expect line 14 and do line 30

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,7 +2,6 @@ require './spec/spec_helper'
 
 RSpec.describe Ship do
   before(:each) do
-    #using @cruiser here as it's in the pry, we can change it to ship if that makes more sense
     @cruiser = Ship.new("Cruiser", 3)
   end
 
@@ -12,30 +11,30 @@ RSpec.describe Ship do
   end
 
   it 'has a name' do
-    expext(@cruiser.name).to eq("Cruiser")
+    expect(@cruiser.name).to eq("Cruiser")
   end
 
   it 'has a length' do 
     expect(@cruiser.length).to eq(3)
   end
 
-  it 'has a starting health' do
+  xit 'has a starting health' do
     expect(@cruiser.health).to eq(3)
   end
 
   #another describe here if we want to make that uniform
-  it 'returns false if ship is not sunk' do
+  xit 'returns false if ship is not sunk' do
     expect(@cruiser.sunk?).to eq(false)
   end  
 
-  it 'loses health on hit'
+  xit 'loses health on hit' do
     @cruiser.hit
     expect(@cruiser.health).to eq(2)
     @cruiser.hit
     expect(@cruiser.health).to eq(1)
   end
 
-  it 'returns true if ship is sunk' do
+  xit 'returns true if ship is sunk' do
     @cruiser.hit
     @cruiser.hit
     @cruiser.hit
