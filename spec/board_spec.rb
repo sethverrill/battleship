b95f2f72b69aa21ad235ff5263806b322c9da2a6
Alora Riley <alora.riley@gmail.com>
2024-09-06 13:39:07 -0600

    test: Add validate coordinates and ship placement. render: Change formating by adding describe blocks

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,25 +5,58 @@ RSpec.describe Board do
       @board = Board.new  
     end
 
-    it 'exisits' do
-        expect(@board).to be_an_instance_of(Board)
-    end
+    describe '#initialize board' do
+        it 'exisits' do
+            expect(@board).to be_an_instance_of(Board)
+        end
 
-    it 'is a hash' do
-        expect(@board.cells).to be_a(Hash)
-    end
+        it 'is a hash' do
+            expect(@board.cells).to be_a(Hash)
+        end
 
-    it 'is not empty' do
-        expect(@baord.cells).not_to be_empty
-    end
+        it 'is not empty' do
+            expect(@baord.cells).not_to be_empty
+        end
 
-    it 'has 16 cells' do
-        expect(@board.cells.size) to eq(16)
-    end 
+        it 'has 16 cells' do
+            expect(@board.cells.size).to eq(16)
+        end 
 
-    it 'has Cell objects as values' do
-        @board.cells.values.each do |cell| 
-            expect(cell). to be_a(Cell)
+        it 'has Cell objects as values' do
+            @board.cells.values.each do |cell| 
+                expect(cell). to be_a(Cell)
+            end
         end
     end
+
+    describe '#can validate coordinates' do
+        it 'can tell us if a coordinate is on the board' do
+            expect(@board.valid_coordinate?("A1")).to eq(true)
+            expect(@board.valid_coordinate?("D4")).to eq(true)
+            expect(@board.valid_coordinate?("A5")).to eq(false)
+            expect(@board.valid_coordinate?("E1")).to eq(false)
+            expect(@board.valid_coordinate?("A22")).to eq(false)
+        end
+    end
+
+    describe '#can validate ship placements' do
+        before(:each) do
+            @cruiser = Ship.new("Cruiser", 3)
+            @submarine = Ship.new("Submarine, 2")
+        end
+
+            it 'should have number of coordinates in the array equal to length of ship' do
+                expect(@board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+                board.valid_placement?(submarine, ["A2", "A3", "A4"]).to eq(false)
+            end
+
+            it 'should have consecutive coordinates' do
+                expect(@board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+                expect(@board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+                expect(@board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+                expect(@board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+            end
+    end
+
+
 end
