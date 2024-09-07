8062b8cb8fae8581a9eb04e3a3e6fc8e8f2eeacb
Seth Verrill <verrill.seth@gmail.com>
2024-09-07 08:26:59 -0600

    feat: Add code to create Board and cells within the board

---
+++ lib/board.rb
@@ -0,0 +1,19 @@
+class Board
+  attr_reader :cells  
+
+  def initialize
+    @cells = board_cells
+  end
+
+  def board_cells
+    cells = {}
+    ('A'..'D').each do |letter|
+      (1..4).each do |number|
+        coordinate = "#{letter}#{number}"
+        cells[coordinate] = Cell.new(coordinate)        
+      end
+    end
+    cells 
+  end
+  #I think this method will need to be drastically changed once we expand board size
+end
