b55c12f739e2f3e51b52296a8e4fc2dc7d5c8174
Alora Riley <alora.riley@gmail.com>
2024-09-09 20:14:47 -0600

    test: Change player tests to allow() format. Change comp_take_shot to expect(). to include format. Finish feedback tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -10,40 +10,62 @@ RSpec.describe Turn do
       expect(@turn).to be_an_instance_of(Turn)
     end
   end
-    
-  describe '#displays the boards' do
-    it "renders the player board" do
-      expect(@turn.display_board).to eq()
+
+  describe '#player takes a turn' do
+    before(:each) do
+      @board = Board.new 
+      @cruiser = Ship.new("Cruiser", 3)
+      @submarine = Ship.new("Submarine", 2)
     end
+
+      it "player chooses a coordinate and it's a hit" do
+        allow(@turn).to receive(:gets).and_return("A1")
+        @board.place(@cruiser, ["A1", "A2", "A3"])
+
+        expect(@turn.p_take_shot).to eq("H")
+      end
+
+      it "player chooses a coodinate and it's a miss" do
+        allow(@turn).to receive(:gets). and_return("A4")
+        @board.place(@cruiser, ["A1", "A2", "A3"])
+
+        expect(@turn.p_take_shot).to eq("M")
+      end
+
+      it "player chooses a coordinate and it's invalid" do
+        allow(@turn).to receive(:gets). and_return("E6")
+
+        expect(@turn.p_take_shot).to eq("Sorry, that coordinate is not on the board. Try again.")
   end
 
-  describe '#player makes a guess' do
-    it "player chooses a coordinate to fire on" do
-      expect(@turn.p_choose_coord).to eq()
-    end
-  end
-
-  describe '#computer makes a guess' do
+  describe '#computer takes a turn' do
     it "computer chooses a coordiante to fire on" do
-      expect(@turn.comp_choose_coord).to eq()
+      @board.place_computer_ships
+
+      shot = turn.comp_take_shot
+      expect(["H", "M", "X"]).to include(shot)
     end
   end
   
   describe "#gives feedback" do
     it "player had a hit"
-      expect(@turn.)
+      allow(@turn).to receive(:p_take_shot). and_return("A1")
+      expect(@turn.player_feedback).to eq("Your shot on A1 was a hit!")
     end
 
     it "player had a miss" do
-      expect(@turn.)
+      allow(@turn).to receive(:p_take_shot). and_return("A4")
+      expect(@turn.player_feedback).to eq("Your shot on A4 was a miss.")
     end
 
     it "computer had a hit" do
-      expect(@turn.)
+      allow(@turn).to receive(:comp_take_shot).and_return("A1")
+      expect(@turn.computer_feedback).to eq("My shot on A1 was a hit!")
     end
 
     it "computer had a miss" do
-      expect(@turn.)
+      allow(@turn).to receive(:comp_take_shot).and_return("B1")
+      expect(@turn.computer_feedback).to eq("My shot on B1 was a miss.")
     end
   end 
 end
