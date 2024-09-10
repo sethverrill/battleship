248e3857743a72b925a6732663aaf00abb8660cb
Seth Verrill <verrill.seth@gmail.com>
2024-09-10 17:27:12 -0600

    feat: Add method to create ships. Found edge case in health

--- lib/ship.rb
+++ lib/ship.rb
@@ -2,6 +2,7 @@ class Ship
     attr_reader :name,
                 :length,
                 :health
+
     def initialize(name, length)
         @name = name
         @length = length
@@ -9,10 +10,51 @@ class Ship
     end
 
     def hit
-        @health -=1
+        @health -=1 if @health > 0
     end   
 
     def sunk?
         @health ==0
+    end
+
+    def self.gets_input
+        gets.chomp
+    end
+
+    def self.build_ships
+        ships = []
+        puts "Would you like to create custom ships? (y/n)"
+        response = gets_input.downcase
+
+        case response
+        when 'n'
+            ships << Ship.new("Cruiser", 3)
+            ships << Ship.new("Submarine", 2)
+            ships << Ship.new("Dinghy", 1)
+        when 'y'
+            loop do 
+                puts "Enter ship name (or 'done' to finish):"
+                name = gets_input
+                break if name.downcase == 'done'
+
+                puts "Enter ship length:"
+                length_str = gets_input
+                length = length_str.to_i
+
+                if valid_ship?(name, length)
+                    ships << Ship.new(name, length)
+                else
+                    puts "Invalid ship. Please try again"
+                end
+            end
+        else
+            puts "Invalid response. Enter 'y' or 'n'."
+            ships = build_ships
+        end
+        ships
+    end
+
+    def self.valid_ship?(name, length)
+        length > 0 && !name.empty?
     end
 end
